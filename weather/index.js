404a7fc4e70329bead741880bd69a31ce98c34b6
Nguyen Thanh <thanh.nn@mor.com.vn>
2024-10-16 17:25:53 +0700

    fix :fix search error

--- weather/index.js
+++ weather/index.js
@@ -1,6 +1,5 @@
-
-const DEFAULT_VALUE='--';
-const searchInput = $("#search-Input");
+const DEFAULT_VALUE = '--';
+const searchInput = $("#searchInput");
 const cityName = $(".city-name");
 const timeCity = $(".time-city");
 const temperature = $(".temperature");
@@ -9,19 +8,23 @@ const weatherIcon = $(".weather-icon");
 const wind = $(".wind");
 const humidity = $(".humidity");
 
+searchInput.on('change', function(e) {
+    const query = e.target.value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&lang=vi&appid=1a829827882f153ee20946c06bb7bbf4`;
 
-
-searchInput.addEventListener('change', (e)=>{
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&units=metric&lang=vi&appid=1a829827882f153ee20946c06bb7bbf4 `)
-    .then(async res=>{
-        const data =await res.json();
-        console.log('[Search Input]',data);
-        cityName.innerHTML=data.name || DEFAULT_VALUE;
-        status.innerHTML=data.weather[0].description || DEFAULT_VALUE;
-        weatherIcon.setAttribute('src', ` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`) || DEFAULT_VALUE;
-        temperature.innerHTML=Math.round(data.main.temp)|| DEFAULT_VALUE;
-        humidity.innerHTML=data.main.humidity || DEFAULT_VALUE;
-        wind.innerHTML=(data.wind.speed*3.6).toFixed(2) || DEFAULT_VALUE;
-
-    })
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        success: function(data) {
+            cityName.html(data.name || DEFAULT_VALUE);
+            status.html(data.weather[0].description || DEFAULT_VALUE);
+            weatherIcon.attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`) || DEFAULT_VALUE;
+            temperature.html(Math.round(data.main.temp) || DEFAULT_VALUE);
+            humidity.html(data.main.humidity || DEFAULT_VALUE);
+            wind.html((data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE);
+        },
+        error: function() {
+            console.log('Error fetching data');
+        }
+    });
 });
